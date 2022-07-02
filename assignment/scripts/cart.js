511637c26d11f335e1bb782670feb1e0d741d836
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-07-01 20:34:22 -0500

    questions before stretch goals answered.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -11,16 +11,14 @@ function addItem( item ){
 } // end of addItem function.
 
 function listItems(){
-    let i = 0;
-    for (i of basket){
+    for (let i of basket){
         console.log('This is an item in the array:', i);
     }//end of for of loop.
-   
 }// end of listItems function.
 
 function empty(){
     basket = [];
-    console.log('you deleted everything in your cart.');
+    console.log('In the empty function: you deleted everything in your cart.');
 }//end of empty function
 
 console.log(addItem('shoes'));
@@ -28,19 +26,5 @@ console.log(addItem('jeans'));
 console.log(addItem('dress'));
 listItems();
 empty();
-listItems();
 
-/*
 
-- Create a function called `empty`. It should:
-  - reset the `basket` to an empty array
-
-> __IMPORTANT__
-> Make sure that you are writing code *in the file* to test every function that you write!
-
-For example, to test `addItem`:
-```
-console.log(`Basket is ${basket}`);
-console.log('Adding apples (expect true)', addItem('apples'));
-console.log(`Basket is now ${basket}`);
-```*/
